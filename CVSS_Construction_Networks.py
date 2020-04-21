efaa9ca9bb0fc5b1501557ff7fbafc79bea1f645
Bharadwaj Mantha <bmantha@nyu.edu>
2020-04-21 16:02:32 +0400

    Update CVSS_Construction_Networks.py
    included 2nd method for severity score calculations - last column in the output

--- CVSS_Construction_Networks.py
+++ CVSS_Construction_Networks.py
@@ -12,6 +12,7 @@
 
 # general imports 
 import networkx as nx
+import pandas as pd
 
 class participant(object):
     #participant class with respective CVSS base metric attributes
@@ -81,38 +82,38 @@ def propogation(node1, node2):
     # node2 metrics get maximum of each of the metrics of the ancestor node (node 1)
     
     #checking AV
-    minValue = max(metricAV[node1.AV], metricAV[node2.AV])
-    metric = [key  for (key, value) in metricAV.items() if value == minValue]
+    maxValue = max(metricAV[node1.AV], metricAV[node2.AV])
+    metric = [key  for (key, value) in metricAV.items() if value == maxValue]
     node2.AV = metric[0]
     
     #checking AV
-    minValue = max(metricAC[node1.AC], metricAC[node2.AC])
-    metric = [key  for (key, value) in metricAC.items() if value == minValue]
+    maxValue = max(metricAC[node1.AC], metricAC[node2.AC])
+    metric = [key  for (key, value) in metricAC.items() if value == maxValue]
     node2.AC = metric[0]
     
     #checking PR
-    minValue = max(metricPR[node1.PR], metricPR[node2.PR])
-    metric = [key  for (key, value) in metricPR.items() if value == minValue]
+    maxValue = max(metricPR[node1.PR], metricPR[node2.PR])
+    metric = [key  for (key, value) in metricPR.items() if value == maxValue]
     node2.PR = metric[0]    
 
     #checking UI
-    minValue = max(metricUI[node1.UI], metricUI[node2.UI])
-    metric = [key  for (key, value) in metricUI.items() if value == minValue]
+    maxValue = max(metricUI[node1.UI], metricUI[node2.UI])
+    metric = [key  for (key, value) in metricUI.items() if value == maxValue]
     node2.UI = metric[0]  
     
     #checking C
-    minValue = max(metricImpact[node1.C], metricImpact[node2.C])
-    metric = [key  for (key, value) in metricImpact.items() if value == minValue]
+    maxValue = max(metricImpact[node1.C], metricImpact[node2.C])
+    metric = [key  for (key, value) in metricImpact.items() if value == maxValue]
     node2.C = metric[0] 
 
     #checking I
-    minValue = max(metricImpact[node1.I], metricImpact[node2.I])
-    metric = [key  for (key, value) in metricImpact.items() if value == minValue]
+    maxValue = max(metricImpact[node1.I], metricImpact[node2.I])
+    metric = [key  for (key, value) in metricImpact.items() if value == maxValue]
     node2.I = metric[0] 
     
     #checking A
-    minValue = max(metricImpact[node1.A], metricImpact[node2.A])
-    metric = [key  for (key, value) in metricImpact.items() if value == minValue]
+    maxValue = max(metricImpact[node1.A], metricImpact[node2.A])
+    metric = [key  for (key, value) in metricImpact.items() if value == maxValue]
     node2.A = metric[0] 
     
     return None
@@ -128,7 +129,7 @@ def getPathScore(allParticipants):
     # the path with maximum probability is chosen as the critical path for the network configuration
     
     pathScore = 1 # initialize the value 
-    pathScore_list = [] # list which has all the path scores stored
+    pathScore2 = 1 # for method 2 which is product of all node values
     output = [] # list of lists which has paths and resp path scores
     for sourceNode in allParticipants:
         #loop over all the nodes except the target node
@@ -141,9 +142,11 @@ def getPathScore(allParticipants):
                 path_list = []
                 # reset all participant scores to initial inputted values
                 resetToInitial(allParticipants_o,allParticipants)
+                pathScore2 = 1 # reset this to initial value for each path
                 for eachNode in path:
                     #print(eachNode.participant_id)
                     path_list.append(eachNode.participant_id)
+                    pathScore2 = pathScore2* 0.1*getParticipantScore(eachNode)[-1]
                 #print("For the path: ", path_list)
                 pathScore = 1 #default value
                 pathScore = 0.1*pathScore*getParticipantScore(sourceNode)[-1] 
@@ -159,11 +162,18 @@ def getPathScore(allParticipants):
                     #print(path[i].participant_id)
                     #path[i].reset()
                     #print(pathScore)
-                pathScore_list.append(round(pathScore,2))
-                output.append([path_list,round(pathScore,2),get_path_likelihood(path),get_path_score_category(round(pathScore,2)),get_path_likelihood_category(get_path_likelihood(path))])
-                index_max_score = pathScore_list.index(max(pathScore_list))
-                critical_path = output[index_max_score]
-    return output, critical_path
+                path_ll = get_path_ll(path)
+                pathScore1 = round(pathScore,2)
+                pathScore2 = round(pathScore2,2)
+                
+                output.append([path_list,
+                               path_ll, get_path_ll_cat(path_ll),
+                               pathScore1, pathScore2
+                               #get_path_score_cat(pathScore1),
+                               ])
+                #index_max_score = pathScore_list.index(max(pathScore_list))
+                #critical_path = output[index_max_score]
+    return output
 
         
 ################################### Inputs ####################################
@@ -211,7 +221,7 @@ allParticipants_o = [O_o,A_o,APM_o,SI_o,PE_o,ME_o,F_o]
 #allSourceNodes = [NWH,FBL]
 TargetNode = O
 
-def get_path_score_category(score):
+def get_path_score_cat(score):
     if score >= 0.0 and score < 0.40:
         return "Low"
     elif score >= 0.40 and score < 0.70:
@@ -221,7 +231,7 @@ def get_path_score_category(score):
     elif score >=0.90 and score <= 1.00:
         return "Critical"
 
-def get_path_likelihood_category(score):
+def get_path_ll_cat(score):
     if score >= 0.0 and score < 0.40:
         return "Unlikely"
     elif score >= 0.40 and score < 0.70:
@@ -269,7 +279,7 @@ G.add_edges_from([
 
 #total_edge_weight = 1.0
 
-def get_path_likelihood(path):
+def get_path_ll(path):
     current_path_weight = 1
     num_edges = len(path)-1
     for i in range(num_edges):
@@ -282,25 +292,22 @@ def get_path_likelihood(path):
 
 ############### testing and execution ###############################
 
-#testing CVSS v3.1 scores
-#print(getParticipantScore(NWH))
 
+# output the data in a data frame
+print("all paths and scores are: ")
 
-# to test basescore calculations
-print("all_paths and scores are: ")
-#print(getPathScore(allParticipants)[0])
-# print each list in list of lists in a new line
-for s in getPathScore(allParticipants)[0]:
-    print(*s)
-print("")
-print("critical path and score is: ")
-print(getPathScore(allParticipants)[1])
+data = getPathScore(allParticipants)
+df = pd.DataFrame.from_records(data)
+print(df)
+
 #reset values because the last node will not get updated otherwise
 resetToInitial(allParticipants_o,allParticipants) 
-# print the scores and metrics of the participants in the network to cross check
-print("")
 
-for eachPart in allParticipants:
-    print(getParticipantMetric(eachPart))
-    print(getParticipantScore(eachPart))
-    print("")
+# =============================================================================
+# print the scores and metrics of the participants in the network to cross check
+#print("")
+#for eachPart in allParticipants:
+#     print(getParticipantMetric(eachPart))
+#     print(getParticipantScore(eachPart))
+#     print("")
+# =============================================================================
